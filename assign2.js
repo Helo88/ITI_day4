ce993bb9ea9985c43c0e0c47bd465350da5e384f
Israa Hussein <36270987+Helo88@users.noreply.github.com>
2021-11-18 03:04:05 +0200

    Update assign2.js

--- assign2.js
+++ assign2.js
@@ -3,7 +3,7 @@
 // Bonus Assignments: q1
 function PrintHeaders() {
   for (let i = 1; i < 7; i++) {
-    // document.write(`<h${i}> welcome to my page <h${i}>`);
+     document.write(`<h${i}> welcome to my page <h${i}>`);
   }
 }
 
